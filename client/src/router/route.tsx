ac23abfcbe71cf46ead677f756c476fec3b142ff
Ayomide <ayomidelawal700@gmail.com>
2023-01-27 15:26:11 +0100

    reorganize the router configuration

---
+++ client/src/router/route.tsx
@@ -0,0 +1,57 @@
+import { Navigate, RouteObject } from 'react-router-dom';
+import {
+  Cart,
+  Checkout,
+  Home,
+  Login,
+  ProductDetail,
+  Shop,
+  Signup,
+} from '../pages';
+import { Layout } from '../components/Layout';
+import { AuthGrantRoute } from './restricted/AuthGrantRoute';
+
+type LinkPath = Record<
+  string,
+  { path: string; name: string; children?: LinkPath } | { path: `$${'self'}` }
+>;
+
+const path = {
+  home: { name: 'home', path: '/' },
+  '/': { name: 'home', path: '/' },
+  cart: { name: 'cart', path: '/cart' },
+  login: { name: 'login', path: '/login' },
+  signup: { name: 'signup', path: '/signup' },
+  product: {
+    name: 'product',
+    path: '/product',
+    children: { ':id': { path: '$self' } },
+  },
+  checkout: { name: 'checkout', path: '/cart' },
+} satisfies LinkPath;
+
+const routeObject = {
+  element: <Layout />,
+  children: [
+    { path: '/', element: <Home /> },
+    { path: '/home', element: <Navigate to="/" /> },
+    {
+      element: <AuthGrantRoute />,
+      children: [
+        { path: '/login', element: <Login /> },
+        { path: '/signup', element: <Signup /> },
+      ],
+    },
+    {
+      path: '/product',
+      children: [
+        { index: true, element: <Shop /> },
+        { path: ':id', element: <ProductDetail /> },
+      ],
+    },
+    { path: '/cart', element: <Cart /> },
+    { path: '/checkout', element: <Checkout /> },
+  ],
+} satisfies RouteObject;
+
+export { routeObject, path as pathLink };
