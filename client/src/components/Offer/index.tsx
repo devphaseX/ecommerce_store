8110db19bce1ba98f1487744dae17b9708896c84
Ayomide <ayomidelawal700@gmail.com>
2023-01-28 18:18:33 +0100

    start the implementation for the offer component~

--- client/src/components/Offer/index.tsx
+++ client/src/components/Offer/index.tsx
@@ -12,12 +12,11 @@ const Offer = () => {
             <div className="clock_top-content">
               <h3 className="top-content__title">Limited Offer</h3>
               <h4 className="top-content__type">Quality Armchair</h4>
-
+              <Clock date="feb 10, 2023" />
               <Link to="/product" className="store__button-link">
                 <button className="buy_button store__button">Visit shop</button>
               </Link>
             </div>
-            <Clock />
           </div>
           <div className="offer__col offer_col-image">
             <div>
