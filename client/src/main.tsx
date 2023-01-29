78aede496ee42c47262c7aab59707349a70dfa45
Ayomide <ayomidelawal700@gmail.com>
2023-01-30 00:14:16 +0100

    add support for global state

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { globalStore } from './store/setup';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <Provider store={globalStore}>
     <App />
+  </Provider>
 );
