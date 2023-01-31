887dd2405c1acab7f08093615deaa00de51e95b3
Ayomide <ayomidelawal700@gmail.com>
2023-01-31 08:50:41 +0100

    connect the redux store to the component tree

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { globalStore } from './store/setup';
+import { store } from './store/setup';
 import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <Provider store={globalStore}>
+  <Provider store={store}>
     <App />
   </Provider>
 );
