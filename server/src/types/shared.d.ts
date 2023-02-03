150f48c9df4a61ca9a78064f262157f1d9871369
Ayomide <ayomidelawal700@gmail.com>
2023-02-03 11:13:11 +0100

    expose certain frequency types as globally available

---
+++ server/src/types/shared.d.ts
@@ -0,0 +1,58 @@
+import { ZodAny, ZodRawShape, ZodTypeAny } from 'zod';
+
+declare global {
+  type Status = 'failed' | 'success';
+
+  type ResponseBase = {
+    status: Status;
+  };
+
+  type PlainErrorWithCause<ErrorCause> = {
+    cause?: ErrorCause;
+  };
+  type SuccessCase<Data> = {
+    [K in 'data' as unknown extends Data ? never : K]: Data;
+  } & ResponseBase & {
+      status: 'success';
+    };
+
+  type FailCase<
+    Error,
+    WithCause extends boolean = boolean
+  > = boolean extends WithCause
+    ? FailCase<Error, true>
+    : ResponseBase & {
+        status: 'failed';
+        error: { message: string } & (false extends WithCause
+          ? unknown
+          : PlainErrorWithCause<Error>);
+      };
+
+  type ServerGenField = 'createdAt' | 'updatedAt' | 'imgUrl';
+
+  type Expand<T> = T extends infer O ? { [K in keyof O]: O[K] } : never;
+
+  type ExtensibleZodShape<T> = Expand<
+    ZodRawShape & {
+      [K in keyof T]: T[K] extends object
+        ? ExtensibleZodShape<T[K]>
+        : ZodTypeAny;
+    }
+  >;
+
+  type ServerResponse<Data, Error, Dev extends boolean = boolean> =
+    | SuccessCase<Data>
+    | FailCase<Error, Dev>;
+
+  type DataWithId<T> = { _id?: string; id: string } & T;
+
+  type ACTIVE_ON_DEV = ReturnType<
+    typeof getEnvVariable
+  >['NODE_ENV'] extends infer Mode
+    ? 'development' extends Mode
+      ? 'production' extends Mode
+        ? boolean
+        : true
+      : false
+    : boolean;
+}
