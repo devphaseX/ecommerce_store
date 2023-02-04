d4b19b87f98c27eb8b17299a204621c7c09234f8
Ayomide <ayomidelawal700@gmail.com>
2023-02-04 21:21:41 +0100

    fix issue of price not stored on DB

--- server/src/model/product/Product.model.ts
+++ server/src/model/product/Product.model.ts
@@ -3,6 +3,7 @@ import mongoose, { InferSchemaType } from 'mongoose';
 const ProductSchema = new mongoose.Schema(
   {
     productName: { type: String, require: true },
+    price: { type: Number, require: true },
     imgUrl: { type: String, require: true },
     category: { type: String, require: true },
     shortDesc: { type: String, require: true },
